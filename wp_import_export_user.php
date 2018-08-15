534200de3977a7ba25082e5081e2e115e7593d45
raiden <raiden.nalasa@gmail.com>
2018-08-15 21:17:45 +0800

    added new autoload functionality

--- wp_import_export_user.php
+++ wp_import_export_user.php
@@ -19,5 +19,18 @@ define( 'WPIE_VERSION', '1.0.0' );
 define( 'WPIE_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'WPIE_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
 
-require_once( WPIE_PLUGIN_PATH.'/classes/class-wpie-export.php' );
-require_once( WPIE_PLUGIN_PATH.'/classes/class-wpie-import.php' );
+// require_once( WPIE_PLUGIN_PATH.'/classes/class-wpie-export.php' );
+// require_once( WPIE_PLUGIN_PATH.'/classes/class-wpie-import.php' );
+
+function classAutoLoader($class){
+	$class    = strtolower($class);
+	$the_path = WPIE_PLUGIN_PATH."classes/{$class}.php";
+	if(file_exists($the_path)){
+		require_once($the_path);
+	}else{
+		die("This file named {$class}.php was found not man....");
+	}
+}
+spl_autoload_register('classAutoLoader');
+
+$new = new WPIE_Export;
